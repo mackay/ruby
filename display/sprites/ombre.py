b670af212a55a24a0568178cc0b8ea0d63af750b
Doug Mackay <mackay.doug@gmail.com>
2020-02-23 17:35:21 -0800

    blend optimizations for ombre

--- display/sprites/ombre.py
+++ display/sprites/ombre.py
@@ -3,11 +3,12 @@ from display.gradient import pixel_gradient
 
 
 class OmbreColor(DynamicSprite):
-    def __init__(self, from_color, to_color, to_shoulder=0):
+    def __init__(self, from_color, to_color, to_shoulder=0, pixel_block_size=4):
         super(OmbreColor, self).__init__()
         self.from_color = from_color
         self.to_color = to_color
         self.to_shoulder = 0
+        self.pixel_block_size = pixel_block_size
 
     def _pixel_gradient(self, n_pixels):
         return pixel_gradient(self.from_color,
@@ -21,8 +22,16 @@ class OmbreColor(DynamicSprite):
 
         colors_max_index = len(colors) - 1
 
+        previous_pixel = None
         for i in range(0, pixel_buffer_len):
-            pixel_buffer[i].blend( colors[min(i, colors_max_index)] )
+            if i and previous_pixel and int(i) % self.pixel_block_size:
+                pixel_buffer[i].set_color_n( previous_pixel.r_n,
+                                             previous_pixel.g_n,
+                                             previous_pixel.b_n,
+                                             previous_pixel.a_n )
+            else:
+                pixel_buffer[i].blend( colors[min(i, colors_max_index)] )
+                previous_pixel = pixel_buffer[i]
 
         super(OmbreColor, self)._do_render(pixel_buffer)
 
